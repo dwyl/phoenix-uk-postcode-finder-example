b8954d62e3f42f72f3f282af8e55effe036c5c84
Robert Francis <robertefrancis18@gmail.com>
2019-03-11 13:29:51 +0000

    use the users postcode input to search ets cache for nearby stores #8

--- lib/store_finder_web/controllers/page_controller.ex
+++ lib/store_finder_web/controllers/page_controller.ex
@@ -8,13 +8,19 @@ defmodule StoreFinderWeb.PageController do
 
   def nearby_stores(conn, %{"postcode" => postcode, "within" => within}) do
     if valid_postcode_format?(postcode) do
-      # use postcode value to get lat_long. Replace hard coded value
-      lat_long = {51.554031, 0.051446}
+      
+      postcode_char = String.to_charlist(postcode)
+      {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} =
+        :httpc.request(:get, {'https://api.postcodes.io/postcodes/' ++ postcode_char, []}, [], [])
+
+      {:ok, parsed_body} = Jason.decode(body)
+      lat = parsed_body["result"]["latitude"]
+      long = parsed_body["result"]["longitude"]
 
       # converts within from a string to an integer
       {within, _} = Integer.parse(within)
 
-      nearby_stores = Haversine.find_nearest_stores(lat_long, within)
+      nearby_stores = Haversine.find_nearest_stores({lat, long}, within)
 
       render(conn, "store.html", stores: nearby_stores)
     else
@@ -25,7 +31,7 @@ defmodule StoreFinderWeb.PageController do
   # Mention that I got this from dwyl fields
   defp valid_postcode_format?(postcode) do
     regex =
-      Regex.compile(
+      Regex.compile!(
         "^([A-Za-z][A-Za-z]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$"
       )
 
