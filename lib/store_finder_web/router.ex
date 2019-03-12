a6561f6901ee63800c56cd599daf6b9b09be38b6
Robert Francis <robertefrancis18@gmail.com>
2019-03-12 16:19:30 +0000

    remove unused pipeline

--- lib/store_finder_web/router.ex
+++ lib/store_finder_web/router.ex
@@ -9,10 +9,6 @@ defmodule StoreFinderWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", StoreFinderWeb do
     pipe_through :browser
 
