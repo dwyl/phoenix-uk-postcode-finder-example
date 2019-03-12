706c010cec782c0ebce9bf08e663a9050aa4ec35
Robert Francis <robertefrancis18@gmail.com>
2019-03-12 14:22:40 +0000

    update page controller test to reflect changes made to route #10

--- test/store_finder_web/controllers/page_controller_test.exs
+++ test/store_finder_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule StoreFinderWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Search"
   end
 end
